b7542a28cd86f903b620042e02dc7631d0359083
hooman mousavi <hoomanmousavi77@gmail.com>
2022-10-14 00:45:19 +0330

    initializing project

---
+++ webpack.dev.config.js
@@ -0,0 +1,66 @@
+const path=require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports={
+    entry: "./src/js/main.js",
+    output: {
+        path: path.resolve(__dirname,'./dist'),
+        filename: "[name].[contenthash].js",
+        assetModuleFilename: "images/[name][ext]"
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|svg|jpe?g)/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)/,
+                type: 'asset/inline'
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename:"css/[name].[contenthash].css"
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "index.html"
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './dist'),
+        },
+        compress: true,
+        port: 9000,
+        devMiddleware:{
+            writeToDisk:true
+        }
+
+    }
+}
